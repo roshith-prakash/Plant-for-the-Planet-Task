66714d4a8469efe3533d10581a23f7db8cc4f24e
roshith <roshithprakash7@gmail.com>
2024-09-10 13:46:53 +0530

    Working on Cookies

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,5 +1,5 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
+import cookie from 'cookie';
 
 type Data = {
   name: string;
@@ -9,5 +9,16 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  const cookies: {} = req.cookies;
+  console.log(cookies);
+
+  res.setHeader(
+    'Set-Cookie',
+    cookie.serialize('name', 'John Doe', {
+      httpOnly: true,
+      maxAge: 60 * 60,
+      secure: true,
+    })
+  );
   res.status(200).json({ name: 'John Doe' });
 }
