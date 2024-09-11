cedb315cbdcba8338daed1705cd3663aaecae6fa
roshith-prakash <roshithprakash7@gmail.com>
2024-09-11 17:27:39 +0530

    Minor UI updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import 'animate.css';
+
 export default function Home() {
   return (
     <div className="h-screen flex justify-center items-center bg-index3 bg-cover bg-no-repeat">
-      <div className="-translate-y-10 flex flex-col items-center gap-y-5 z-5 bg-white p-10 rounded-xl shadow-xl hover:scale-110 transition-all">
+      <div className="animate__animated animate__fadeInUp -translate-y-10 flex flex-col items-center gap-y-5 z-5 bg-white p-10 rounded-xl shadow-xl hover:scale-110 transition-all">
         <img
           src="https://res.cloudinary.com/do8rpl9l4/image/upload/v1725986240/plant_d1wkt7.png"
           alt="Logo"
